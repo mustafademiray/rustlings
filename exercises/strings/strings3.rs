fcbe8e066544a98e96166c25e7c524f4cba788d7
mustafademiray <mustafademiray@protonmail.com>
2023-08-13 04:25:28 +0300

    39/96

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -12,7 +12,7 @@ fn trim_me(input: &str) -> String {
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.to_owned() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
